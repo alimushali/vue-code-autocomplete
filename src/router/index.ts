2fb67b82f41c17339331dc4a8cea73e4e6511ffb
alimushali <iakochenkov@gmail.com>
2023-08-14 08:46:13 +0300

    removed some of template vue code

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MainView from '../views/MainView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'MainView',
+      component: MainView
+    },
+    {
+      path: '/home',
       name: 'home',
       component: HomeView
     },
---
+++ src/views/MainView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="main-window">
+    <input type="text" class="main-window--text" />
+   </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  data: () => ({
+    text: "",
+  }),
+});
+</script>
+
+<style scoped lang="scss">
+.main-window {
+  &--text {
+    width: 100%;
+  }
+}
+</style>
